b45c8000c0294b7f1ad5c5b41797bbed1592874e
NCC Music <ncardu@proton.me>
2026-09-24 01:50:38 -0300

    Corrige el ancho del campo User

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_PREFIX = 'ncc-music-';
-const CACHE_NAME = `${CACHE_PREFIX}shell-v45`;
+const CACHE_NAME = `${CACHE_PREFIX}shell-v46`;
 const APP_SHELL = [
     '/',
     '/index.html',
     '/manifest.webmanifest',
-    '/styles.css?v=20260924userwidth',
+    '/styles.css?v=20260924userwidth2',
     '/assets/app-icons/icon-192-v2.png',
     '/assets/app-icons/icon-512-v2.png',
     '/assets/app-icons/icon-maskable-512-v2.png',
